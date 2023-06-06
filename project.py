91d6db9012b6d438aa23bfea9195baabad0d1a56
Chebrolu Niranjan <125688401+chebro1eu@users.noreply.github.com>
2023-06-06 12:35:07 +0530

    Update project.py

--- project.py
+++ project.py
@@ -11,3 +11,30 @@ df2 = df.iloc[:,:23]
 print(df2.shape)
 print(df2.isnull().sum())
 
+#Hot deck imputation being run
+df3 = pd.DataFrame(df2)
+
+#Function to do Hot deck imputation
+# Function to perform hot deck imputation
+def hot_deck_imputation(df):
+    for col in df.columns:
+        missing_indices = df[col].isnull()  # Get indices of missing values
+        
+        for i in missing_indices[missing_indices].index:
+            non_missing_vals = pd.to_numeric(df.loc[~missing_indices, col])  # Convert to numeric
+            
+            # Find the nearest observed value to the missing value
+            nearest_val = non_missing_vals.iloc[(non_missing_vals - pd.to_numeric(df.loc[i, col])).abs().argsort()[0]]
+            
+            # Replace the missing value with the nearest observed value
+            df.loc[i, col] = nearest_val
+    
+    return df
+
+# Perform hot deck imputation
+
+imputed_df = hot_deck_imputation(df3)
+print(imputed_df)
+
+imputed_df.to_csv(r'C:\Users\Niranjan\Desktop\BISAG\machine failed\cleaned_data.csv', index = False)
+imputed_df.to_excel(r'C:\Users\Niranjan\Desktop\BISAG\machine failed\cleaned_data.xlsx', index = False)
